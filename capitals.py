7811a032146b2cc21e5a45b61ec218c62ff59447
Jordan Freundlich <75767764+Jfreundlich01@users.noreply.github.com>
2022-07-18 08:15:38 -0400

    done

---
+++ capitals.py
@@ -0,0 +1,190 @@
+import random
+
+states = [
+{
+    "name": "Alabama",
+    "capital": "Montgomery"
+}, {
+    "name": "Alaska",
+    "capital": "Juneau"
+}, {
+    "name": "Arizona",
+    "capital": "Phoenix"
+}, {
+    "name": "Arkansas",
+    "capital": "Little Rock"
+}, {
+    "name": "California",
+    "capital": "Sacramento"
+ }
+, {
+    "name": "Colorado",
+    "capital": "Denver"
+}, {
+    "name": "Connecticut",
+    "capital": "Hartford"
+}, {
+    "name": "Delaware",
+    "capital": "Dover"
+}, {
+    "name": "Florida",
+    "capital": "Tallahassee"
+}, {
+    "name": "Georgia",
+    "capital": "Atlanta"
+}, {
+    "name": "Hawaii",
+    "capital": "Honolulu"
+}, {
+    "name": "Idaho",
+    "capital": "Boise"
+}, {
+    "name": "Illinois",
+    "capital": "Springfield"
+}, {
+    "name": "Indiana",
+    "capital": "Indianapolis"
+}, {
+    "name": "Iowa",
+    "capital": "Des Moines"
+}, {
+    "name": "Kansas",
+    "capital": "Topeka"
+}, {
+    "name": "Kentucky",
+    "capital": "Frankfort"
+}, {
+    "name": "Louisiana",
+    "capital": "Baton Rouge"
+}, {
+    "name": "Maine",
+    "capital": "Augusta"
+}, {
+    "name": "Maryland",
+    "capital": "Annapolis"
+}, {
+    "name": "Massachusetts",
+    "capital": "Boston"
+}, {
+    "name": "Michigan",
+    "capital": "Lansing"
+}, {
+    "name": "Minnesota",
+    "capital": "St. Paul"
+}, {
+    "name": "Mississippi",
+    "capital": "Jackson"
+}, {
+    "name": "Missouri",
+    "capital": "Jefferson City"
+}, {
+    "name": "Montana",
+    "capital": "Helena"
+}, {
+    "name": "Nebraska",
+    "capital": "Lincoln"
+}, {
+    "name": "Nevada",
+    "capital": "Carson City"
+}, {
+    "name": "New Hampshire",
+    "capital": "Concord"
+}, {
+    "name": "New Jersey",
+    "capital": "Trenton"
+}, {
+    "name": "New Mexico",
+    "capital": "Santa Fe"
+}, {
+    "name": "New York",
+    "capital": "Albany"
+}, {
+    "name": "North Carolina",
+    "capital": "Raleigh"
+}, {
+    "name": "North Dakota",
+    "capital": "Bismarck"
+}, {
+    "name": "Ohio",
+    "capital": "Columbus"
+}, {
+    "name": "Oklahoma",
+    "capital": "Oklahoma City"
+}, {
+    "name": "Oregon",
+    "capital": "Salem"
+}, {
+    "name": "Pennsylvania",
+    "capital": "Harrisburg"
+}, {
+    "name": "Rhode Island",
+    "capital": "Providence"
+}, {
+    "name": "South Carolina",
+    "capital": "Columbia"
+}, {
+    "name": "South Dakota",
+    "capital": "Pierre"
+}, {
+    "name": "Tennessee",
+    "capital": "Nashville"
+}, {
+    "name": "Texas",
+    "capital": "Austin"
+}, {
+    "name": "Utah",
+    "capital": "Salt Lake City"
+}, {
+    "name": "Vermont",
+    "capital": "Montpelier"
+}, {
+    "name": "Virginia",
+    "capital": "Richmond"
+}, {
+    "name": "Washington",
+    "capital": "Olympia"
+}, {
+    "name": "West Virginia",
+    "capital": "Charleston"
+}, {
+    "name": "Wisconsin",
+    "capital": "Madison"
+}, {
+    "name": "Wyoming",
+    "capital": "Cheyenne"
+}
+]
+
+def game():
+    print("""Let's Learn our State Capitals! 
+We're going to create a game to help us memorize the names of the capitals of all 50 states.""")
+
+
+    random.shuffle(states)
+    count = 0
+    answered = 0
+    for state in states:
+        print("What is the capital of " + state["name"] + "?")
+    
+        answer = input()
+        if answer == state["capital"].capitalize():
+            print(answer)
+            print('Correct!')
+            count += 1
+            answered += 1
+            #print(f'You have gotten {count} capitals correct!')
+        else:
+            print(answer)
+            print('Wrong!')
+            answered += 1
+            #print(f'You have gotten {count} capitals correct!')
+        print("You got " + str(count) + " out of " + str(answered) + " correct " + str((count/answered)* 100) + "%.")
+
+
+        if answered == len(states):
+            play_again = input("Would you like to play again? (y/n)")
+            if play_again == "y":
+                game()
+            else:
+                print("Thanks for playing!")
+game()
